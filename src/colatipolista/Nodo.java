381bdd7b8cd8573d2643a8eb421a5a4ead73c468
Huevo131 <ryardox@gmail.com>
2024-10-02 10:42:41 -0600

    se suben archivos

---
+++ src/colatipolista/Nodo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colatipolista;
+
+/**
+ *
+ * @author yahir
+ */
+public class Nodo {
+    int informacion;
+    Nodo siguiente;
+    
+}
